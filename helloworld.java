31a693d570b599706e495d767bdbcda3f35f6725
MadhuRamiah <madhuramiah@gmail.com>
2017-07-20 16:32:31 -0700

    update 1

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,6 @@
 public class helloworld{
   public static void main(String args[]){
-    System.out.println("Hello world");
+    String name="Hello";
+    System.out.println("Hello world "+name);
   }
 }
